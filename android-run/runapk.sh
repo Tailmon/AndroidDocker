8467ea23f0faad0e6e7d4d6e4727973c089c5293
Pablo Sanabria <pablo.s.q1.4.1991@gmail.com>
2016-07-04 01:46:26 -0400

    Added docker for android and android-run

---
+++ android-run/runapk.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -z ${1+x} || -z ${2+x} || -z ${3+x} ]]; then
+	echo "Usage: ./runapk.sh PATH_TO_APK PACKAGE_NAME MAIN_ACTIVITY"
+	exit 1
+fi
+echo $PATH
+emulator64-x86 -avd "API23-x86" &
+PID=$!
+bootanim=""
+failcounter=0
+until [[ "$bootanim" =~ "stopped" ]]; do
+   bootanim=`adb -e shell getprop init.svc.bootanim 2>&1`
+   if [[ "$bootanim" =~ "not found" ]]; then
+      let "failcounter += 1"
+      if [[ $failcounter -gt 10 ]]; then
+        echo "  Failed to start emulator"
+        exit 1
+      fi
+   fi
+   sleep 1
+done
+adb install -r $1
+adb shell am start -a android.intent.action.MAIN -n $2/.$3
+wait $PID
